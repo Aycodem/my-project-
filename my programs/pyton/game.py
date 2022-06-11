fcb2b3f1357527351fc2be92572071f8aa9184e8
Aycodem <106999740+Aycodem@users.noreply.github.com>
2022-06-11 02:00:35 +0100

    Add files via upload

---
+++ my programs/pyton/game.py
@@ -0,0 +1,7 @@
+number = 20
+
+if(number > 10):
+    print("number is greater than ten")
+else:
+    print("number is not greater than ten")
+
---
+++ my programs/tryer.js
@@ -0,0 +1,77 @@
+// class me {
+//     constructor(name,job,hobby){
+//         this.name=name;
+//         this.job=job;
+//         this.hobby=hobby;
+
+//     }
+// }
+// var x=new me("hello","am","Babalola")
+// console.log(x);
+
+
+
+// funtion me(name,job,hobby)
+// {
+//     this.name=name;
+//     this.job=job;
+//     this.hobby=hobby;
+// }
+// me.prototype.getname= funtion(){
+//     return this.name;
+// }
+// me.prototype.getjob =function () {
+//     return this.job;
+// }
+// var me1 =new me("azeez","babalola","Ayomide");
+// var me2= new me("akande","good","GainNode");
+// document.write(me1);
+
+
+// function one()
+// {
+// console.log("hello");
+// }
+// function two(){
+//     console.log("Ayo,"+"Welcome to programming world");
+// };
+
+// one();
+// setTimeout(two,2000);
+
+
+class user {
+constructor(name,user,online ){
+    this.name=name;
+    this.user=user;
+    this.online=online;
+}
+}
+var user1=new user("Ayo","good",true);
+
+console.log(user1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
